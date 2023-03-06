1c15f7b52a8e01372fdfec53ca70c63f218b1688
Chandru loganathan <78639242+chandrulogan@users.noreply.github.com>
2023-03-06 12:17:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -135,4 +135,4 @@ app.get('/api/customers', (req, res) => {
 });
 
 // Start server
-app.listen(4000, () => console.log('Server started on port 4000'));
+app.listen(process.env.PORT || 4000, () => console.log('Server started on port 4000'));
